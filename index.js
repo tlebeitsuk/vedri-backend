d85dbe17bf6490f5b80392443f3bb7ffc642b5ab
tobias.lebeitsuk <tobias@iterlabs.fi>
2019-12-15 12:26:13 +0100

    refactoring

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
+app = express()
+port = process.env.PORT || 5000
 
 const cors = require('cors')
 app.use(cors())
 
 const addHours = require('date-fns/addHours')
-const startOfHour = require('date-fns/startOfHour')
+startOfHour = require('date-fns/startOfHour')
 
-const { getDataSMHI, parseDataSMHI } = require('./smhi')
-const { getDataYR, parseDataYR } = require('./yr')
-const { getDataFMI, parseDataFMI } = require('./fmi')
+const { getDataSMHI, parseDataSMHI } = require('./lib/smhi')
+const { getDataYR, parseDataYR } = require('./lib/yr')
+const { getDataFMI, parseDataFMI } = require('./lib/fmi')
 
 app.get('/forecast/lat/:lat/lon/:lon', async (req, res) => {
   const dataSMHI = await getDataSMHI(req.params.lat, req.params.lon)
-  const dataYR = await getDataYR(req.params.lat, req.params.lon)
-  const dataFMI = await getDataFMI(req.params.lat, req.params.lon)
+  dataYR = await getDataYR(req.params.lat, req.params.lon)
+  dataFMI = await getDataFMI(req.params.lat, req.params.lon)
 
   let result = {}
 
+  // forecast for 48 hours
   for (let i = 1; i < 49; i++) {
-    let time = addHours(startOfHour(new Date()), i)
+    const time = addHours(startOfHour(new Date()), i)
 
     result[i] = {
       time: time,
@@ -32,13 +33,13 @@ app.get('/forecast/lat/:lat/lon/:lon', async (req, res) => {
   res.send(result)
 })
 
-function notFound(req, res, next) {
+const notFound = (req, res, next) => {
   const error = new Error('Not found')
   res.status(404)
   next(error)
 }
 
-function errorHandler(error, req, res, next) {
+const errorHandler = (error, req, res, next) => {
   res.status(res.statusCode || 500)
   res.json({
     message: error.message
